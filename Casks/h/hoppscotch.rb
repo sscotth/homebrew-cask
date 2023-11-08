99c3234875766e1c2eba2966f33aa5c8a8e742f1
Scott Humphries <git@sscotth.io>
2023-11-08 13:51:12 -0600

    Add Hoppscotch.app v23.8.3-1

---
+++ Casks/h/hoppscotch.rb
@@ -0,0 +1,24 @@
+cask "hoppscotch" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "23.8.3-1"
+  sha256 arm:   "10844de40383483c83e2572b65ac0ae079e079dfdadcd611b464aaff5321a119",
+         intel: "4578ed90780ad93a88f54100386264df7e4b4d70f40f5812705dca842742bc7d"
+
+  url "https://github.com/hoppscotch/releases/releases/download/v#{version}/Hoppscotch_mac_#{arch}.dmg",
+      verified: "github.com/hoppscotch/releases/"
+  name "hoppscotch"
+  desc "Open source API development ecosystem"
+  homepage "https://hoppscotch.com/"
+
+  depends_on macos: ">= :high_sierra"
+
+  app "Hoppscotch.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.hoppscotch.desktop/",
+    "~/Library/Caches/io.hoppscotch.desktop",
+    "~/Library/Saved Application State/io.hoppscotch.desktop.savedState",
+    "~/Library/WebKit/io.hoppscotch.desktop",
+  ]
+end
